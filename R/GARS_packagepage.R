e4dbf35387a42e0f3716f28d77336ebe4e24f6c6
BioinfoMonzino <mattia.chiesa@cardiologicomonzino.it>
2018-03-01 10:52:11 +0100

    updates version 0.99.4

---
+++ R/GARS_packagepage.R
@@ -0,0 +1,20 @@
+#' GARS package for a robust feature selection of high-dimensional data
+#'
+#' The main function of GARS is \code{\link{GARS_GA}}, which implements a
+#' clustering-based Genetic Algorithm to select
+#' Robust Subsets of features in high-dimensional datasets.
+#'  The user can extract the results of
+#'  \code{\link{GARS_GA}}, exploiting the assessor methods:
+#'  \code{\link{MatrixFeatures}}, \code{\link{LastPop}},
+#'  \code{\link{AllPop}} and \code{\link{FitScore}}.
+#'
+#' See the package vignette, by typing \code{vignette("GARS")} to discover
+#' all the \code{\link{GARS_GA}} functions.
+#'
+#' @author Mattia Chiesa, Giada Maioli, Luca Piacentini
+#'
+#' @docType package
+#' @name GARS
+#' @aliases GARS
+#' @keywords package
+NULL
